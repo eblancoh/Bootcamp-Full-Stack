918e4cc170767a488bcfe4445af5b99597f049a2
eblancoh <enrique.blancohenriquez@telefonica.com>
2021-12-02 19:49:29 +0100

    New features added as requested.

---
+++ ide_intro/test_copilot.js
@@ -0,0 +1,20 @@
+// Nodejs server that serves a hello world page
+
+
+// Load the http module to create an http server.
+var http = require('http');
+
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World\n");
+    }
+);
+
+
+// Listen on port 8000, IP defaults to
+//
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://localhost:8000/`);
+  });
